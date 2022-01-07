0b6a3aa64cf85cd9a76110ce684403dbee117a6c
Jibe7 <84497196+Jibe7@users.noreply.github.com>
2022-01-07 10:00:31 +0100

    Point 2 for gettftp

--- gettftp.c
+++ gettftp.c
@@ -1,8 +1,39 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define BUF_SIZE 500
 
 void gettftp(char *host, char *file) {
+
+    struct addrinfo hints;
+    struct addrinfo *result;
+    char hbuf[BUF_SIZE];
+    char sbuf[BUF_SIZE];
+
+   /* Obtain address(es) matching host/port */
+
+    memset(&hints, 0, sizeof(struct addrinfo)); // fill memory with constant byte : void *memset(void *s, int c, size_t n);
+    hints.ai_family = AF_INET;    /* Allow IPv4 */
+    hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+    hints.ai_flags = 0;
+    hints.ai_protocol = IPPROTO_UDP;          /* UDP protocol */
+
+    int s = getaddrinfo(host, "1069", &hints, &result);
+	if (s != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+        exit(EXIT_FAILURE);
+    }
+    getnameinfo(result->ai_addr, result->ai_addrlen,hbuf, sizeof(hbuf), sbuf, sizeof(sbuf), NI_NUMERICHOST | NI_NUMERICSERV);
+      printf("IP : %s \n port :%s \n",hbuf,sbuf); //hbuf IP, sbuf port
+
+   
+
+   freeaddrinfo(result);           /* No longer needed */
 
 }
 
